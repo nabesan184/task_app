82188b3c5050c09299560cb5bdb89cc371081a8c
nabesan184 <simeji.gm@gmail.com>
2019-07-30 22:29:38 +0900

    インデント修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,14 @@
 Task.create(
   title: "課題を提出する",
   body: "現在取り組んでいる Rails アプリの課題を先生に提出する。\n 提出方法は Github にプッシュ後、プッシュしたリポジトリの URL を連絡する。",
-  due_date: "2019-12-30")
+  due_date: "2019-12-30"
+)
 
 Task.create(
   title: "高橋さんに連絡する",
   body: "見積もりの件で金額を下げてもらうために交渉する。\n 駄目だった場合は直接会って土下座をする。",
-  due_date: "2019-8-30")
+  due_date: "2019-8-30"
+)
 
   Task.create(
   title: "ゴミを出す",
